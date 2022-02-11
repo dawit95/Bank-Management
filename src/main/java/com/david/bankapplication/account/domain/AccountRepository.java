4b0d423123cd541a06553c1b7b9dce86ddd0fc72
dawit95 <dawit0310@gmail.com>
2022-02-11 16:28:17 +0900

    [ init ] Add Entity Repository

---
+++ src/main/java/com/david/bankapplication/account/domain/AccountRepository.java
@@ -0,0 +1,12 @@
+package com.david.bankapplication.account.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * FileName : AccountRepository
+ * Author : David
+ * Date : 2022-02-11
+ * Description : Account Entity DB Layer accessor
+ */
+public interface AccountRepository extends JpaRepository<Account, Long> {
+}
---
+++ src/main/java/com/david/bankapplication/account/domain/TransactionLogRepository.java
@@ -0,0 +1,12 @@
+package com.david.bankapplication.account.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * FileName : TransactionLogRepository
+ * Author : David
+ * Date : 2022-02-11
+ * Description : TransactionLog Entity DB Layer accessor
+ */
+public interface TransactionLogRepository extends JpaRepository<TransactionLog, Long> {
+}
