415498c56b3f8cb936a6778a912a7529f89de3e6
dawit95 <dawit0310@gmail.com>
2022-02-11 16:55:09 +0900

    [ feat ] Set timezone

--- src/main/java/com/david/bankapplication/BankApplication.java
+++ src/main/java/com/david/bankapplication/BankApplication.java
@@ -2,9 +2,21 @@ package com.david.bankapplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
+@EnableJpaAuditing
 @SpringBootApplication
 public class BankApplication {
+
+    @PostConstruct
+    public void started() {
+        // timezone Asia/Seoul 셋팅
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(BankApplication.class, args);
