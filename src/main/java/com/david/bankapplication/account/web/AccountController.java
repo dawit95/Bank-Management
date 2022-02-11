a00960fdea4e005a78a6d5fc79a191120e33f228
dawit95 <dawit0310@gmail.com>
2022-02-11 16:12:51 +0900

    [ feat ] Add AccountController

--- src/main/java/com/david/bankapplication/account/web/AccountController.java
+++ src/main/java/com/david/bankapplication/account/web/AccountController.java
@@ -1,8 +1,17 @@
 package com.david.bankapplication.account.web;
 
+import com.david.bankapplication.account.dto.RegisterRequestDto;
+import com.david.bankapplication.account.dto.TransferRequestDto;
+import com.david.bankapplication.global.dto.SuccessResponseDto;
+import com.david.bankapplication.global.service.ResponseGenerateService;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,4 +27,25 @@ import org.springframework.web.bind.annotation.RestController;
 public class AccountController {
     Logger logger = LoggerFactory.getLogger(AccountController.class);
 
+    private final ResponseGenerateService responseGenerateService;
+
+    @ApiOperation(value = "계좌등록",notes = "성공시 요청한 은행에 계좌를 생성하고 계좌번호 반환!")
+    @PostMapping("/register")
+    public ResponseEntity<SuccessResponseDto> register(@RequestBody RegisterRequestDto requestDto) {
+        logger.debug("AccountController 계좌 등록 in Param : {}", requestDto);
+
+        SuccessResponseDto successResponseDto = responseGenerateService.generateSuccessResponse("noneObject");
+
+        return new ResponseEntity<>(successResponseDto, HttpStatus.OK);
+    }
+
+    @ApiOperation(value = "계좌이체",notes = "계좌이체 성공시 거래 번호와 결과 반환!")
+    @PostMapping("/transfer")
+    public ResponseEntity<SuccessResponseDto> transfer(@RequestBody TransferRequestDto requestDto) {
+        logger.debug("AccountController 계좌 이체 in Param : {}",requestDto);
+
+        SuccessResponseDto successResponseDto = responseGenerateService.generateSuccessResponse("noneObject");
+
+        return new ResponseEntity<>(successResponseDto, HttpStatus.OK);
+    }
 }
