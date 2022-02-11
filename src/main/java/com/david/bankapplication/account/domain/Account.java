6d9e37ee333cc6d14b26b8a3034c62c30291a946
dawit95 <dawit0310@gmail.com>
2022-02-11 13:39:30 +0900

    [ init ] Add AccountEntity

---
+++ src/main/java/com/david/bankapplication/account/domain/Account.java
@@ -0,0 +1,45 @@
+package com.david.bankapplication.account.domain;
+
+import com.david.bankapplication.global.util.BaseTime;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+/**
+ * FileName : Account
+ * Author : David
+ * Date : 2022-02-11
+ * Description : 사용자가 등록한 계좌 DB
+ */
+@Getter
+@NoArgsConstructor
+@Entity
+public class Account extends BaseTime {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long pk;
+
+    @Column( nullable = false)
+    private Long userId;
+
+    @Column(nullable = false)
+    private String bankCode;
+
+    @Column(nullable = false)
+    private Long bankAccountNumber;
+
+    @Column
+    private Long bankAccountId;
+
+    @Builder
+    public Account(Long userId,String bankCode,Long bankAccountNumber, Long bankAccountId){
+        this.userId = userId;
+        this.bankCode = bankCode;
+        this.bankAccountNumber = bankAccountNumber;
+        this.bankAccountId = bankAccountId;
+    }
+
+}
